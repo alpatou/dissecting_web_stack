e09f8c8f9322f43efbd4d8a72cd4794f8a55a0dd
patal <patourasalexandros@gmail.com>
2021-10-29 01:33:07 +0300

    server with get is ok

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 a late night
-i got myself reading this
+i got myself reading this: https://www.thedigitalcatonline.com/blog/2020/02/16/dissecting-a-web-stack/
 and the cat said 
 
 Create the file server.py and type this code. Yes, type it, don't just copy and paste, you will not learn anything otherwise.
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import re ## why not response? 
 
 
 ## create a socket instance
@@ -15,10 +16,38 @@ s.bind(('', 8080))
 s.listen()
 
 
+HEAD_200 ="HTTP/1.1 200 OK\n\n" 
+HEAD_404 ="HTTP/1.1 404 Not Found\n\n" 
+
 ## serve forever you slave socket
 while True: ## and that is final the usefulness of such weird capabilities of languages 
     # accept the connection, accept your fate
     conn, addr = s.accept() ## this is i suppose something list() on php, google it (?)
 
-    # re
+    # receive data from this socket using a bugger of 1024 bytes
+    data = conn.recv(1024)
+
+    # print out then the data nicely decoded
+    request = data.decode('utf-8')
+
+    #print(request)
+    print(request)
+    resource = re.match(r'GET /(.*) HTTP', request).group(1) # group (?)
+    try:
+        with open(resource, 'r') as f:
+            content = HEAD_200 + f.read()
+        print('Resource {} correctly served'.format(resource))
+    except FileNotFoundError:
+        content = HEAD_404 + 'Resource {} cannot be found'.format(resource)
+        print('Resource {} cannot be loaded'.format(resource))
+
+    print('--------------------------------') # do not forget to be artistic
+
+    #we have content now to send
+    conn.sendall(bytes(content, 'utf-8'))
+
+    # close the connection but why closing since it has to run forever, but a suppose the 
+    # essence of the server is the socket, connections we can have many, so it will wait until 
+    # receiving data and then be closed again, Lets test it
+    conn.close()
 
